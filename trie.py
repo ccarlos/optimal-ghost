89656329cc905bae6a12380f4b985e6d3ca6fdbc
Cesar Carlos <cesar.g.carlos@gmail.com>
2012-05-01 15:34:41 -0700

    [Add] Class implementation of a Trie data structure.
    List and tuple based Trie.
    
    * insert_key(k, v)
    * tr_has_key(k)
    * retrieve_val(k)
    * start_with_prefix(prefix)

--- trie.py
+++ trie.py
@@ -1,11 +1,12 @@
 ### *********************************************************
+### http://vkedco.blogspot.com/2012/02/trie-in-python.html
 ### Module: trie.py
 ### Description: A list and tuple based Python implementation
 ### of the Trie data structure that supports the following
 ### methods:
 ### 1. insert_key(k, v, trie) - insert a non-empty string key k
 ###    into trie and associate k with value v
-### 2. has_key(k, trie) - True if trie has key k
+### 2. tr_has_key(k, trie) - True if trie has key k
 ### 3. retrieve_val(k, trie) - retrieve value indexed under key
 ###    k in trie
 ### 4. start_with_prefix(prefix, trie) - return the list of
@@ -14,190 +15,126 @@
 ### Bugs, comments to vladimir dot kulyukin at gmail dot com
 ### *********************************************************
 
-### ***************** HELPER FUNCTIONS **********************
 
-def is_trie_bucket(x):
-    return isinstance(x, tuple) and \
-           len(x) == 2 and \
-           isinstance(x[0], str) and \
-           isinstance(x[1], list) and \
-           len(x[1]) == 1
+class Trie:
+    def __init__(self):
+        """Construct and object of type Trie."""
+        self.tr = [[]]
 
-def is_trie_branch(x):
-    return isinstance(x, list)
+    ### ***************** HELPER FUNCTIONS **********************
 
-def get_bucket_key(b):
-    return b[0]
+    def is_trie_bucket(self, x):
+        return isinstance(x, tuple) and \
+               len(x) == 2 and \
+               isinstance(x[0], str) and \
+               isinstance(x[1], list) and \
+               len(x[1]) == 1
 
-def get_bucket_val(b):
-    return b[1][0]
+    def is_trie_branch(self, x):
+        return isinstance(x, list)
 
-### ******************** INSERT_KEY ************************
+    def get_bucket_key(self, b):
+        return b[0]
 
-def insert_key(k, v, trie):
-    ## do not insert empty keys
-    if k == '':
-        return None
-    ## if trie has k or stores it with the same value v,
-    ## do not insert
-    elif has_key(k, trie) and retrieve_val(k, trie) == v:
-        return None
-    else:
-        tr = trie
-        ## for each character c in k, find a child
-        ## branch that starts with c
-        for c in k:
-            branch = find_child_branch(tr, c)
-            ## if there is no branch that starts with c,
-            ## create it and append it at the end of
-            ## the current level.
-            if branch == None:
-                new_branch = [c]
-                tr.append(new_branch)
-                tr = new_branch
-            else:
-                tr = branch
-        ## tr is now bound to the branch, so insert
-        ## a new bucket.
-        tr.append((k,[v]))
-        return None
+    def get_bucket_val(self, b):
+        return b[1][0]
 
-## a branch is either empty or it is a list whose first
-## element is a character and the rest are buckets or
-## sub-branches.
-def get_child_branches(trie):
-    if trie == []:
-        return []
-    else:
-        return trie[1:]
+    ### ******************** INSERT_KEY ************************
 
-def find_child_branch(trie, c):
-    for branch in get_child_branches(trie):
-       if branch[0] == c:
-           return branch
-    return None
-
-### ************************ HAS_KEY *************************
-       
-def has_key(k, trie):
-    br = retrieve_branch(k, trie)
-    if br == None:
-        return False
-    else:
-        return is_trie_bucket(get_child_branches(br)[0])
-
-### ******************** RETRIEVE_VAL ************************
-
-## find a branch in trie that is indexed under k.
-def retrieve_branch(k, trie):
-    if k == '':
-        return None
-    else:
-        tr = trie
-        for c in k:
-            br = find_child_branch(tr, c)
-            if br == None:
-                return None
-            else:
-                tr = br
-        return tr
-
-## find a branch and retrieve its bucket, second element.
-def retrieve_val(k, trie):
-    if not has_key(k, trie): return None
-    br = retrieve_branch(k, trie)
-    return get_bucket_val(br[1])
-
-### *************** START_WITH_PREFIX ************************
-
-def start_with_prefix(prefix, trie):
-    ## 1. find the branch indexed by prefix
-    br = retrieve_branch(prefix, trie)
-    if br == None: return []
-
-    key_list = []
-    q = get_child_branches(br)
-    ## 2. go through the sub-branches of the
-    ## branch indexed by the prefix and
-    ## collect the bucket strings into key_list
-    while not q == []:
-        curr_br = q.pop(0)
-        if is_trie_bucket(curr_br):
-            key_list.append(get_bucket_key(curr_br))
-        elif is_trie_branch(curr_br):
-            q.extend(get_child_branches(curr_br))
+    def insert_key(self, k, v):
+        ## do not insert empty keys
+        if k == '':
+            return None
+        ## if trie has k or stores it with the same value v,
+        ## do not insert
+        elif self.tr_has_key(k) and self.retrieve_val(k) == v:
+            return None
         else:
-            return 'ERROR: bad branch'
-    return key_list
+            tr = self.tr
+            ## for each character c in k, find a child
+            ## branch that starts with c
+            for c in k:
+                branch = self.find_child_branch(tr, c)
+                ## if there is no branch that starts with c,
+                ## create it and append it at the end of
+                ## the current level.
+                if branch == None:
+                    new_branch = [c]
+                    tr.append(new_branch)
+                    tr = new_branch
+                else:
+                    tr = branch
+            ## tr is now bound to the branch, so insert
+            ## a new bucket.
+            tr.append((k, [v]))
+            return None
 
-### ************************** TESTS **************************
+    ## a branch is either empty or it is a list whose first
+    ## element is a character and the rest are buckets or
+    ## sub-branches.
+    def get_child_branches(self, trie):
+        if trie == []:
+            return []
+        else:
+            return trie[1:]
 
-### construct a trie from http://en.wikipedia.org/wiki/Trie
-def test_01():
-    tr = [[]]
-    insert_key('to', 7, tr)
-    insert_key('tea', 3, tr)
-    insert_key('ted', 4, tr)
-    insert_key('ten', 12, tr)
-    insert_key('A', 15, tr)
-    insert_key('i', 11, tr)
-    insert_key('in', 5, tr)
-    insert_key('inn', 9, tr)
-    return tr
+    def find_child_branch(self, trie, c):
+        for branch in self.get_child_branches(trie):
+            if branch[0] == c:
+                return branch
+        return None
 
-### test start_with_prefix on the trie returned by test_01()
-def test_02():
-    tr = test_01()
-    print start_with_prefix('t', tr)
-    print start_with_prefix('te', tr)
-    print start_with_prefix('i', tr)
+    ### ************************ TR_HAS_KEY *************************
 
+    def tr_has_key(self, k):
+        br = self.retrieve_branch(k)
+        if br == None:
+            return False
+        else:
+            return self.is_trie_bucket(self.get_child_branches(br)[0])
 
-the_tent = '''
-            THE TENT
-        Jalaluddin Rumi
+    ### ******************** RETRIEVE_VAL ************************
 
-Outside, the freezing desert night.
-This other night inside grows warm, kindling.
-Let the landscape be covered with thorny crust.
-We have a soft garden in there.
-The continents blasted,
-cities and little towns, everything
-become a scorched, blackened ball.
-The news we hear is full of grief for that future,
-but the real news inside here
-is there's no news at all.
+    ## find a branch in trie that is indexed under k.
+    def retrieve_branch(self, k):
+        if k == '':
+            return None
+        else:
+            tr = self.tr
+            for c in k:
+                br = self.find_child_branch(tr, c)
+                if br == None:
+                    return None
+                else:
+                    tr = br
+            return tr
 
-Translated from the Persian by Coleman Barks
-'''
+    ## find a branch and retrieve its bucket, second element.
+    def retrieve_val(self, k):
+        if not self.tr_has_key(k):
+            return None
+        br = self.retrieve_branch(k)
+        return self.get_bucket_val(br[1])
 
-import string
+    ### *************** START_WITH_PREFIX ************************
 
-### remove punctuation and '\n' from txt
-### and lowercase all characters
-def normalize_text(txt):
-    ntxt = txt.strip()
-    for pc in string.punctuation:
-        ntxt = ntxt.replace(pc, ' ')
-    ntxt = ntxt.replace('\n', ' ')
-    ntxt = ntxt.lower()
-    return ntxt
+    def start_with_prefix(self, prefix):
+        ## 1. find the branch indexed by prefix
+        br = self.retrieve_branch(prefix)
+        if br == None:
+            return []
 
-### construct a trie whose keys are words in
-### the normalized text of Rumi's poem 'The Tent'
-def test_03():
-    tent_normalized = normalize_text(the_tent)
-    words = tent_normalized.split(' ')
-    tr = [[]]
-    for w in words:
-        insert_key(w, True, tr)
-    return tr
-
-### try a few calls with start_with_prefix on
-### the trie constructed in test_03()
-def test_04():
-    tr = test_03()
-    print start_with_prefix('th', tr)
-    print start_with_prefix('b', tr)
-    print start_with_prefix('bl', tr)
-    print start_with_prefix('bla', tr)
+        key_list = []
+        q = self.get_child_branches(br)
+        ## 2. go through the sub-branches of the
+        ## branch indexed by the prefix and
+        ## collect the bucket strings into key_list
+        while not q == []:
+            curr_br = q.pop(0)
+            if self.is_trie_bucket(curr_br):
+                key_list.append(self.get_bucket_key(curr_br))
+            elif self.is_trie_branch(curr_br):
+                q.extend(self.get_child_branches(curr_br))
+            else:
+                return 'ERROR: bad branch'
+        return key_list
