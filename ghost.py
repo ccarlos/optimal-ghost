5f6c10fab5a0b9df65a2071995899b4215132df2
Cesar Carlos <cesar.g.carlos@gmail.com>
2012-05-03 14:52:00 -0700

    [Add] Docstrings and AI functionality.

--- ghost.py
+++ ghost.py
@@ -1,5 +1,7 @@
+from random import choice
 from string import ascii_lowercase
 from sys import argv, exit, stderr
+from time import sleep
 
 from trie import Trie
 
@@ -10,6 +12,8 @@ class OptimalGhost:
 
     # NOTE: Assumes file_name has one word per line as found in WORD.LST.
     def load_words(self, file_name):
+        """Load Trie data structure with words of len >= 4."""
+
         ins_key = self.tr.insert_key
         with open(file_name, 'r') as f:
             for line in f.readlines():
@@ -17,16 +21,56 @@ class OptimalGhost:
                 if len(line) >= 4:
                     ins_key(line, True)
 
+    def computer_letter_choice(self, curr_word, turn):
+        """Optimally choose the next letter in the game.
+
+        At the current game state we will keep track of the possible
+        letter choices to advance using Trie.start_with_prefix. Our initial
+        choice is to choose a letter in a word(odd length) that will increase
+        our chances of winning, otherwise we will choose a word(even length)
+        with maximal length to keep the game going.
+        """
+
+        print "Computer's Turn ->"
+        sleep(1.0)
+        win_wrds = self.tr.start_with_prefix(curr_word)
+        odd_wrds = [w for w in win_wrds if len(w) % 2 == 1]
+        evn_wrds = [w for w in win_wrds if len(w) % 2 == 0]
+        evn_wrds.sort(key=len, reverse=True)
+
+        word = ''
+        if odd_wrds:
+            word = choice(odd_wrds)
+            if len(word) == turn + 1:
+                word = choice(odd_wrds)
+        elif evn_wrds:
+            word = evn_wrds[0]
+        else:
+            word = choice(ascii_lowercase)
+
+        next_letter = word[turn] if len(word) > 1 else word[0]
+
+        return next_letter
+
     def boot_game(self):
-        player_turn = 'me'
+        """Start an interactive game of Optimal Ghost.
+
+        A real player starts first and afterwards it is the computer's
+        turn. After either entity has entered a choice we will check whether
+        they have completed a word or if they have entered a letter that
+        doesn't allow to advance a game.
+        """
+
+        player_turn = 'Player.'
         curr_word = ''
-        print "Welcome to Optimal Ghost!"
+        turn = 0
+        print "\nWelcome to Optimal Ghost!\n"
 
         while(True):
             print 'Current Word: {0}'.format(curr_word)
 
-            if player_turn == 'me':
-                next_letter = raw_input("Enter a letter: ").strip()
+            if player_turn == 'Player':
+                next_letter = raw_input("Enter a letter -> ").strip()
                 if next_letter not in ascii_lowercase:
                     print "Please enter a lower case letter."
                     continue
@@ -34,19 +78,25 @@ class OptimalGhost:
                     print "Please enter a single letter."
                     continue
                 curr_word = curr_word + next_letter
-            else:
-                # TODO: Input AI functionality.
-                print "comp turn::"
+            else:  # Computer turn
+                next_letter = self.computer_letter_choice(curr_word, turn)
+                print 'Computer chooses letter: {0}'.format(next_letter)
+                curr_word = curr_word + next_letter
 
-            #check valid word
+            # check valid word
             if self.tr.tr_has_key(curr_word):
-                print '{0} Loses:'.format(player_turn)
+                print '*** {0} Loses ***'.format(player_turn)
+                print '*** Completed word: {0}'.format(curr_word)
                 break
 
-            #TODO: check invalid word + cannot advance, eg allx
-            #if set is empty() of children of possible next words
+            # check invalid word + cannot advance, eg allx
+            if not self.tr.start_with_prefix(curr_word):
+                print '*** {0} Loses ***'.format(player_turn)
+                print '*** Invalid word, cannot advance: {0}'.format(curr_word)
+                break
 
-            player_turn = 'comp' if player_turn == 'me' else 'me'
+            turn = turn + 1
+            player_turn = 'Computer' if player_turn == 'Player' else 'Player'
 
 
 def main(argv):
@@ -56,20 +106,29 @@ def main(argv):
     See README for problem description.
 
     main() will read in lines from inputfile. Valid lines will be parsed and
-    update the current object.
+    update the current object. After initialization boot_game() will be called
+    and launch a game of optimal ghost.
     """
+
     if len(argv) != 1:
         print >> stderr, "Insufficient number of arguments."
         print >> stderr, "Usage: ghost.py filename"
         exit(2)
 
     file_name = argv[0]
-    opt = OptimalGhost()
 
+    opt = OptimalGhost()
     opt.load_words(file_name)
 
-    #TODO: implement a way to let players keep playing.
-    opt.boot_game()
+    while True:
+        opt.boot_game()
+        q = raw_input("Would you like to quit? [Y|y or N|n]:").strip()
+        if q == 'Y' or q == 'y':
+            break
+        elif q == 'N' or q == 'n':
+            continue
+        else:
+            print 'Invalid input: You must play again with the ghost'
 
 
 if __name__ == '__main__':
